9f10b997ae5b90bd65ec24784a0d575d58e5b6b8
Sundus Munir <sundasmkhan91@gmail.com>
2024-01-09 17:01:58 -0800

    initialstep

---
+++ book/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+  <>  
+  <h1>Bookish</h1>
+<p>  A book without philosophy, without a sense of mission or wider dream, is just words on a page, let's not call them books.</p>
+ /</> 
+  );
+}
+
+export default App;
