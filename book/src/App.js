f3208342d018d6fc2cc99dde2a6962abdc3a636d
Sundus Munir <sundasmkhan91@gmail.com>
2024-01-09 19:17:25 -0800

    added child compo

--- book/src/App.js
+++ book/src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-
+// App.js
+import BookList from './components/booklist';
 function App() {
-  return (
-  <>  
-  <h1>Bookish</h1>
-<p>  A book without philosophy, without a sense of mission or wider dream, is just words on a page, let's not call them books.</p>
- /</> 
+
+  
+return (
+    <>  
+      <h1>Bookish</h1>
+      <p>A book without philosophy, without a sense of mission or wider dream, is just words on a page, let's not call them books.</p>
+      <BookList />  
+    
+    </> 
   );
-}
+};
+
 
 export default App;
---
+++ book/src/components/booklist.js
@@ -0,0 +1,16 @@
+// components/BookList.js
+import React from 'react';
+
+export default function Booklist() { 
+  return (
+    <div>
+    <ul>
+    <li>'The Chronicles of Narnia'</li>
+    <li>'The Girl on the Train'</li> 
+    <li>The Hobbit'</li>
+    <li>'Harry Potter and the Sorcerers Stone'</li>
+    </ul>
+     </div>
+  );
+  };
+
