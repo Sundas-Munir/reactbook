c206356681eb15908f2d0d0df8d453b67cec8866
Sundus Munir <sundasmkhan91@gmail.com>
2024-01-09 19:41:48 -0800

    intro to props

--- book/src/App.js
+++ book/src/App.js
@@ -1,13 +1,19 @@
 // App.js
 import BookList from './components/booklist';
-function App() {
-
+import React, { useState } from 'react';
+  function App() {
+    const [books, setBooks] = useState([
+      'The Chronicles of Narnia',
+      'The Girl on the Train',
+      'The Hobbit',
+      'Harry Potter and the Sorcerers Stone',
+    ]);
   
 return (
     <>  
       <h1>Bookish</h1>
       <p>A book without philosophy, without a sense of mission or wider dream, is just words on a page, let's not call them books.</p>
-      <BookList />  
+     <BookList book={books} />
     
     </> 
   );
--- book/src/components/booklist.js
+++ book/src/components/booklist.js
@@ -1,16 +1,16 @@
 // components/BookList.js
 import React from 'react';
 
-export default function Booklist() { 
+export default function Booklist(props) { 
+ 
   return (
     <div>
     <ul>
-    <li>'The Chronicles of Narnia'</li>
-    <li>'The Girl on the Train'</li> 
-    <li>The Hobbit'</li>
-    <li>'Harry Potter and the Sorcerers Stone'</li>
+    <li>{props.book[0]}</li>
+        <li>{props.book[1]}</li>
+        <li>{props.book[2]}</li>
+        <li>{props.book[3]}</li>
     </ul>
      </div>
   );
-  };
-
+};
